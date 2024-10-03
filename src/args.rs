2a120c2545c7f041099d3973de5bd7e26526f3ca
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2024-10-03 16:38:52 -0400

    Implemented arg loader - Implemented GameBuilder - Implemented wasd & arrow controls for PlayerController - Implemented ReaderState - Created get_args to convert args into GameBuilder - Created read_args that converts List of str into GameBuilder and to test diffrent inputs

--- src/args.rs
+++ src/args.rs
@@ -3,11 +3,11 @@ use std::env;
 
 use bevy::prelude::KeyCode;
 
-#[derive(Debug, )]
-struct GameBuilder {
-    render: bool,
-    player_1: Option<bool>,
-    player_2: Option<bool>,
+#[derive(Debug, PartialEq, Eq)]
+pub struct GameBuilder {
+    pub render: bool,
+    pub player_1: PlayerController,
+    pub player_2: PlayerController,
 }
 impl Default for GameBuilder {
     fn default() -> Self {
@@ -20,7 +20,7 @@ impl Default for GameBuilder {
 }
 
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 enum PlayerController {
     Server{
         ip: IpAddr,
@@ -39,48 +39,150 @@ enum PlayerController {
 }
 impl PlayerController {
     pub fn wasd() -> PlayerController {
-        todo!();
-        PlayerController{
-
+        PlayerController::Control{
+            move_forward: KeyCode::KeyW,
+            move_backward: KeyCode::KeyS,
+            rotate_left: KeyCode::KeyA,
+            rotate_right: KeyCode::KeyD,
+            spin_turret_left: KeyCode::KeyQ,
+            spin_turret_right: KeyCode::KeyE,
+            shoot: KeyCode::Space,
         }
     }
     pub fn arrow() -> PlayerController{
-        todo!();
+        PlayerController::Control{
+            move_forward: KeyCode::ArrowUp,
+            move_backward: KeyCode::ArrowDown,
+            rotate_left: KeyCode::ArrowLeft,
+            rotate_right: KeyCode::ArrowRight,
+            spin_turret_left: KeyCode::KeyI,
+            spin_turret_right: KeyCode::KeyO,
+            shoot: KeyCode::KeyP,
+        }
     }
 }
 
 #[derive(Debug)]
-enum State{
-    In,
-    Out,
+enum ReaderState{
+    Render,
+    Player1,
+    Player2,
     None
 }
 pub fn get_args() -> GameBuilder {
-    let mut state = State::None;
-    let args = env::args();
+    read_args(env::args().into_iter())
+}
 
-    let mut input: Option<String> = None;
-    let mut output: Option<String> = None;
+fn read_args<'a, I: Iterator<Item = String>>(args: I) -> GameBuilder {
+    let mut state: ReaderState = ReaderState::None;
 
-    for arg in args {
-        todo!()
-        // match (&arg as &str, &mut state, &mut input, &mut output) {
-        //     ("-i", state, None, _) => *state = State::In,
-        //     ("-o", state, _, None) => *state = State::Out,
+    let mut builder: GameBuilder = GameBuilder::default();
 
-        //     (arg, State::In, Some(input), _) => input.extend(format!(" {arg}").chars()),
-        //     (arg, State::In, input, _) => *input = Some(arg.into()),
+    for arg in args{
+        let arg = arg.as_str();
+        match (arg, &mut state) {
+            ("-render" | "-r", ReaderState::None) => {
+                state = ReaderState::Render
+            },
+            ("-p1" | "-player_1", ReaderState::None) => state = ReaderState::Player1,
+            ("-p2" | "-player_2", ReaderState::None) => state = ReaderState::Player2,
+
+            ("t" | "true", ReaderState::Render) => builder.render = true,
+            ("f" | "false", ReaderState::Render) => builder.render = false,
+
+            ("wasd", ReaderState::Player1) => builder.player_1 = PlayerController::wasd(),
+            ("arrow", ReaderState::Player1) => builder.player_1 = PlayerController::arrow(),
+            (ip, ReaderState::Player1) => {
+                let ip = ip.split(":").collect::<Vec<&str>>();
+
+                let tmp = match ip[..] {
+                    [ip, port] => PlayerController::Server{
+                        ip: {
+                            let ip = ip.split(".").collect::<Vec<&str>>();
+
+                            match ip[..]{
+                                [a, b, c, d] => {
+                                    let a = a.parse().unwrap();
+                                    let b = b.parse().unwrap();
+                                    let c = c.parse().unwrap();
+                                    let d = d.parse().unwrap();
+
+                                    Ipv4Addr::new(a, b, c, d).into()
+                                },
+                                [a, b, c, d, e, f, g, h] => {
+                                    let a = a.parse().unwrap();
+                                    let b = b.parse().unwrap();
+                                    let c = c.parse().unwrap();
+                                    let d = d.parse().unwrap();
+                                    let e = e.parse().unwrap();
+                                    let f = f.parse().unwrap();
+                                    let g = g.parse().unwrap();
+                                    let h = h.parse().unwrap();
+
+                                    Ipv6Addr::new(a, b, c, d, e, f, g, h).into()
+                                }
+                                _ => panic!("Invalid IP")
+                            }
+                        },
+                        port: port.parse::<u16>().unwrap()
+                    },
+                    _ => panic!("Invalid format")
+                };
+
+                builder.player_1 = tmp;
+            },
+
+            ("wasd", ReaderState::Player2) => builder.player_2 = PlayerController::wasd(),
+            ("arrow", ReaderState::Player2) => builder.player_2 = PlayerController::arrow(),
+            (ip, ReaderState::Player2) => {
+                let ip = ip.split(":").collect::<Vec<&str>>();
+
+                let tmp = match ip[..] {
+                    [ip, port] => PlayerController::Server{
+                        ip: {
+                            let ip = ip.split(".").collect::<Vec<&str>>();
+
+                            match ip[..]{
+                                [a, b, c, d] => {
+                                    let a = a.parse().unwrap();
+                                    let b = b.parse().unwrap();
+                                    let c = c.parse().unwrap();
+                                    let d = d.parse().unwrap();
+
+                                    Ipv4Addr::new(a, b, c, d).into()
+                                },
+                                [a, b, c, d, e, f, g, h] => {
+                                    let a = a.parse().unwrap();
+                                    let b = b.parse().unwrap();
+                                    let c = c.parse().unwrap();
+                                    let d = d.parse().unwrap();
+                                    let e = e.parse().unwrap();
+                                    let f = f.parse().unwrap();
+                                    let g = g.parse().unwrap();
+                                    let h = h.parse().unwrap();
+
+                                    Ipv6Addr::new(a, b, c, d, e, f, g, h).into()
+                                }
+                                _ => panic!("Invalid IP")
+                            }
+                        },
+                        port: port.parse::<u16>().unwrap()
+                    },
+                    _ => panic!("Invalid format")
+                };
+
+                builder.player_2 = tmp;
+            },
             
-        //     (arg, State::Out, _, Some(output)) => output.extend(format!(" {arg}").chars()),
-        //     (arg, State::Out, _, output) => *output = Some(arg.into()),
-
-        //     (_arg, State::None, None, None) => {}
-            
-        //     state => {
-        //         panic!("{state:#?}")
-        //     }
-        // }
+            state => {
+                panic!("{state:#?}")
+            }
+        }
     }
 
-    Some((input?,output?))
+    builder
+}
+
+mod tests{
+
 }
